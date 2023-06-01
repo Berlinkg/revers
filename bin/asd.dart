2eca30a81ada9f25c3162f8d974dc784f6a84c2d
Berlinkg <nadyrbekov570@gmail.com>
2023-06-01 23:45:00 +0600

    revers

---
+++ bin/asd.dart
@@ -0,0 +1,7 @@
+List<int> numbers = [1, 2, 3, 4, 5];
+List<int> reversedNumbers = [];
+for (int i = numbers.length - 1; i >= 0; i--) {
+  reversedNumbers.add(numbers[i]);
+}
+
+print(reversedNumbers); // [5, 4, 3, 2, 1]
